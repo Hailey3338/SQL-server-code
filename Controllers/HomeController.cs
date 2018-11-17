10ed30abc2248489b9e43e4ad520b52e5e9e1063
Hailey3338 <Hailey3338@hotmail.com>
2018-11-17 18:03:59 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Web.DataModel;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly XingEntities _context = new XingEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SearchUser(string searchName, int? searchAgeFrom, int? searchAgeTo, string searchHobby)
+        {
+
+            var users = _context.sp_SearchUser(searchName, searchAgeFrom, searchAgeTo, searchHobby).ToList();
+            return View(users);
+        }
+        //[HttpGet]
+        //public ActionResult SearchUser()
+        //{
+        //    return View();
+        //}
+
+    }
+}
