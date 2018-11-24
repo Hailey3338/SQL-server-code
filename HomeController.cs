544850e8494ab71bd399773cfa5903113f4c8f00
Hailey3338 <Hailey3338@hotmail.com>
2018-11-24 21:50:18 +0100

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebQuery.Models;
+
+namespace WebQuery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly XingEntities _Context = new XingEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var results = _Context.Classes.Include(t => t.User).ToList();
+            return View(results);
+        }
+    }
+}
